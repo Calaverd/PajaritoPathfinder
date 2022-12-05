a574c405ef31d8d0744d64a7710ea0482a6714fc
Calaverd <bfxdosval@gmail.com>
2022-12-05 00:07:20 -0600

    Refactoring Heap to lua class

--- src/heap.lua
+++ src/heap.lua
@@ -1,9 +1,10 @@
 --- A simple data agnostic Heap implementation.
 -- This heap is build to be data agnostic, so it can work
 -- comparing arbitrary data types. Is used for the priority queue
--- @classmod Heap
--- @author Calaverd
--- @license MIT
+--@classmod Heap
+--@author Calaverd
+--@license MIT
+local Heap = {}
 
 -- Define the math functions for some small speed bonus
 local FMOD = math.fmod
@@ -13,139 +14,154 @@ if not FMOD then -- Why are you usign lua < 5.1 ?
     FMOD = function(a, b) return a - FLOOR(a / b) * b end
 end
 
---- Returns a new Heap instance
--- @return Heap instance
-return function()
-    local self = {}
-
-    local last = 0 -- id of the last inserted item
-    local container = {} -- body of the heap
-
-    --- A internal function that compares two things.
-    --  This function is used to handle the internally comparisions
-    -- between items. You can overrride it with your own function
-    -- as long it returns a boolean
-    -- @param object_a
-    -- @param object_b
-    -- @return bool True if object_a has more priority than object_b, False otherwise.
-    function self.compare(object_a, object_b)
-        return object_a <= object_b
-    end
-
-    --- Clears the contents of the heap.
-    -- Clears the heap from any content, so it can be reused.
-    function self.clear()
-        container = {}
-        last = 0
-    end
-
-    --- Intenral function, gets the parent of a node.
-    -- @int id the id of a node in the heap.
-    -- @return int id of parent node.
-    local function getParentOf(id)
-        if FMOD(id, 2) == 0 then return id / 2 end
-        return (id - 1) / 2
-    end
-
-    --- Adds a new item into the Heap.
-    --  Use this function to insert new items into the heap
-    -- and sort it according to their priority.
-    --  As long as the item can get compared with the ones
-    -- already on the heap, anything goes.
-    -- @param data anything to be added.
-    function self.push(data)
-        --is first element.
-        if last <= 1 then
-            container[1] = data
-            last = 2
-            return nil
-        end
-
-        local heap_property = false
-        local current = last
-        local parent = 0
-        container[current] = data
-
-        while not heap_property do
-            parent = getParentOf(current)
-            if self.compare(container[current], container[parent]) then
-                --swap current and parent
-                container[current] = container[parent]
-                container[parent] = data
-                current = parent
-            else
-                heap_property = true
-            end
-            if current <= 1 then
-                heap_property = true
-            end
-        end
-        last = last + 1
-    end
-
-    --- Remove and retrive the higher priority item.
-    --  This function takes the item with higher priority on
-    -- the heap, removes it, and then returns it.
-    -- @return data, nil if heap is empty
-    function self.pop()
-        --sawp first and last value
-        local top = container[1]
-        container[1] = container[last - 1]
-        container[last - 1] = nil
-        local current = 1
-        local heap_property = false
-        while not heap_property do
-            local left = 2 * current
-            local right = 2 * current + 1
-            local choosed = current
-            -- Exist the right node?
-            if container[right] then
-                if self.compare(container[left], container[right]) then
-                    choosed = left
-                else
-                    choosed = right
-                end
-                if self.compare(container[choosed], container[current]) then
-                    --swap the value
-                    local temp = container[choosed]
-                    container[choosed] = container[current]
-                    container[current] = temp
-                    current = choosed
-                else
-                    heap_property = true
-                end
-                -- Exist the left node?
-            elseif container[left] then
-                if self.compare(container[left], container[current]) then
-                    --swap the value
-                    choosed = left
-                    local temp = container[choosed]
-                    container[choosed] = container[current]
-                    container[current] = temp
-                    current = choosed
-                else
-                    heap_property = true
-                end
-            else
-                heap_property = true
-            end
-        end
-
-        last = MMAX(last - 1, 0)
-        return top
-    end
-
-    -- Returns the higher priority item and keeps it on the heap.
-    -- @return data the higer priority element on the heap.
-    function self.peek()
-        return container[1]
-    end
-
-    --- Gets the number of items in the heap.
-    -- @return int the number of items in the heap.
-    function self.getSize()
-        return MMAX(last - 1, 0)
-    end
-
-    return self
+--- Internal function, gets the parent of a node.
+--@int id the id of a node in the heap.
+--@return int id of parent node.
+local function getParentOf(id)
+    if FMOD(id, 2) == 0 then return id / 2 end
+    return (id - 1) / 2
 end
+
+--- Constructs a new Heap instance
+--@return Heap
+function Heap:new()
+    local obj = {
+        -- index of the last inserted item
+        last = 0,
+        -- body of the heap, contains items
+        container = {},
+        -- An internal function used for comparisions
+        compareFun = function (a,b) return a <= b end
+    }
+
+    setmetatable(obj, self)
+    self.__index = self
+    return obj
+end
+
+--- A internal function that compares two things.
+--  This function is used to handle the internally comparisions
+-- between items. You can overrride it with your own function
+-- as long it returns a boolean
+--@see setCompare
+--@param object_a
+--@param object_b
+--@treturn bool True if object_a has more priority than object_b, False otherwise.
+function Heap:compare(object_a, object_b)
+    return self.compareFun( object_a, object_b )
+end
+
+--- Takes a new function to use to compare.
+--@param function newCompare new function to compare values.
+function Heap:setCompare(newCompare)
+    self.compareFun = newCompare or self.compareFun
+end
+
+--- Clears the contents of the heap.
+-- Clears the heap from any content, so it can be reused.
+function Heap:clear()
+    self.container = {}
+    self.last = 0
+end
+
+--- Adds a new item into the Heap.
+--  Use this function to insert new items into the heap
+-- and sort it according to their priority.
+--  As long as the item can get compared with the ones
+-- already on the heap, anything goes.
+--@param data anything to be added.
+function Heap:push(data)
+    --is first element.
+    if self.last <= 1 then
+        self.container[1] = data
+        self.last = 2
+        return nil
+    end
+
+    local heap_property = false
+    local current = self.last
+    local parent = 0
+    self.container[current] = data
+
+    while not heap_property do
+        parent = getParentOf(current)
+        if self:compare(self.container[current], self.container[parent]) then
+            --swap current and parent
+            self.container[current] = self.container[parent]
+            self.container[parent] = data
+            current = parent
+        else
+            heap_property = true
+        end
+        if current <= 1 then
+            heap_property = true
+        end
+    end
+    self.last = self.last + 1
+end
+
+--- Remove and retrive the higher priority item.
+--  This function takes the item with higher priority on
+-- the heap, removes it, and then returns it.
+--@return data, nil if heap is empty
+function Heap:pop()
+    --sawp first and last value
+    local top = self.container[1]
+    self.container[1] = self.container[self.last - 1]
+    self.container[self.last - 1] = nil
+    local current = 1
+    local heap_property = false
+    while not heap_property do
+        local left = 2 * current
+        local right = 2 * current + 1
+        local choosed = current
+        -- Exist the right node?
+        if self.container[right] then
+            if self:compare(self.container[left], self.container[right]) then
+                choosed = left
+            else
+                choosed = right
+            end
+            if self:compare(self.container[choosed], self.container[current]) then
+                --swap the value
+                local temp = self.container[choosed]
+                self.container[choosed] = self.container[current]
+                self.container[current] = temp
+                current = choosed
+            else
+                heap_property = true
+            end
+            -- Exist the left node?
+        elseif self.container[left] then
+            if self:compare(self.container[left], self.container[current]) then
+                --swap the value
+                choosed = left
+                local temp = self.container[choosed]
+                self.container[choosed] = self.container[current]
+                self.container[current] = temp
+                current = choosed
+            else
+                heap_property = true
+            end
+        else
+            heap_property = true
+        end
+    end
+
+    self.last = MMAX(self.last - 1, 0)
+    return top
+end
+
+-- Returns the higher priority item and keeps it on the heap.
+--@return data the higer priority element on the heap.
+function Heap:peek()
+    return self.container[1]
+end
+
+--- Gets the number of items in the heap.
+--@return int the number of items in the heap.
+function Heap:getSize()
+    return MMAX(self.last - 1, 0)
+end
+
+return Heap
--- test/heap.test.lua
+++ test/heap.test.lua
@@ -7,107 +7,118 @@ describe("Heap test", function()
   local myHeap
 
   setup( function()
-    local newHeap = require("heap")
-    myHeap = newHeap();
+    local Heap = require("heap")
+    myHeap = Heap:new();
   end )
   after_each(function()
-    myHeap.clear()
+    myHeap:clear()
   end)
   teardown(function()
     myHeap = nil
   end)
 
   it("Starting size must be 0", function()
-    assert.are.equal(myHeap.getSize(), 0)
+    assert.are.equal(myHeap:getSize(), 0)
   end)
 
   it("Push changes the size of heap", function()
-    assert.are.equal(myHeap.getSize(), 0)
-    myHeap.push(1);
-    assert.are.equal(myHeap.getSize(), 1)
-    myHeap.push(2)
-    myHeap.push(3);
-    myHeap.push(4);
-    myHeap.push(5);
-    assert.are.equal(myHeap.getSize(),5);
+    assert.are.equal(myHeap:getSize(), 0)
+    myHeap:push(1);
+    assert.are.equal(myHeap:getSize(), 1)
+    myHeap:push(2)
+    myHeap:push(3);
+    myHeap:push(4);
+    myHeap:push(5);
+    assert.are.equal(myHeap:getSize(),5);
+  end)
+
+  it("Clear remove all items", function()
+    assert.are.equal(myHeap:getSize(), 0)
+    myHeap:push(1);
+    myHeap:push(2)
+    myHeap:push(3);
+    myHeap:push(4);
+    myHeap:push(5);
+    myHeap:clear();
+    assert.are.equal(myHeap:getSize(),0);
+    assert.are.equal(myHeap:peek(),nil);
+    assert.are.equal(myHeap:pop(),nil);
   end)
 
   it("Pop changes the size of heap", function()
-    assert.are.equal(myHeap.getSize(), 0)
-    myHeap.push(1);
-    myHeap.push(2)
-    myHeap.push(3);
-    myHeap.push(4);
-    myHeap.push(5);
+    assert.are.equal(myHeap:getSize(), 0)
+    myHeap:push(1);
+    myHeap:push(2)
+    myHeap:push(3);
+    myHeap:push(4);
+    myHeap:push(5);
     
-    assert.are.equal(myHeap.getSize(), 5)
-    myHeap.pop();
-    assert.are.equal(myHeap.getSize(), 4);
+    assert.are.equal(myHeap:getSize(), 5)
+    myHeap:pop();
+    assert.are.equal(myHeap:getSize(), 4);
     
-    myHeap.pop();
-    myHeap.pop();
-    myHeap.pop();
-    myHeap.pop();
-    assert.are.equal(myHeap.getSize(), 0);
+    myHeap:pop();
+    myHeap:pop();
+    myHeap:pop();
+    myHeap:pop();
+    assert.are.equal(myHeap:getSize(), 0);
   end)
 
   it("Pop returns items in acording priority", function()
-    myHeap.push(2)
-    myHeap.push(500);
-    myHeap.push(1);
-    myHeap.push(-1);
-    myHeap.push(3);
+    myHeap:push(2)
+    myHeap:push(500);
+    myHeap:push(1);
+    myHeap:push(-1);
+    myHeap:push(3);
     
-    assert.are.equal(myHeap.pop(), -1)
-    assert.are.equal(myHeap.pop(), 1)
-    assert.are.equal(myHeap.pop(), 2)
-    assert.are.equal(myHeap.pop(), 3)
-    assert.are.equal(myHeap.pop(), 500)
+    assert.are.equal(myHeap:pop(), -1)
+    assert.are.equal(myHeap:pop(), 1)
+    assert.are.equal(myHeap:pop(), 2)
+    assert.are.equal(myHeap:pop(), 3)
+    assert.are.equal(myHeap:pop(), 500)
   end)
 
   it("Peek returns item of higger priority, does not change size.", function()
-    myHeap.push(2)
-    myHeap.push(500);
-    myHeap.push(1);
-    myHeap.push(-1);
-    myHeap.push(3);
+    myHeap:push(2)
+    myHeap:push(500);
+    myHeap:push(1);
+    myHeap:push(-1);
+    myHeap:push(3);
     
-    assert.are.equal(myHeap.getSize(), 5)
-    assert.are.equal(myHeap.peek(), -1)
-    assert.are.equal(myHeap.getSize(), 5)
+    assert.are.equal(myHeap:getSize(), 5)
+    assert.are.equal(myHeap:peek(), -1)
+    assert.are.equal(myHeap:getSize(), 5)
   end)
 
   it("Custom compare function", function()
-    myHeap.compare = function (b,a)
-      return b >= a
-    end
-    myHeap.push(2)
-    myHeap.push(500);
-    myHeap.push(1);
-    myHeap.push(-1);
-    myHeap.push(3);
+    myHeap:setCompare( function (b,a) return b >= a end )
+    myHeap:push(2)
+    myHeap:push(500);
+    myHeap:push(1);
+    myHeap:push(-1);
+    myHeap:push(3);
     
-    assert.are.equal(myHeap.pop(), 500)
-    assert.are.equal(myHeap.pop(), 3)
-    assert.are.equal(myHeap.pop(), 2)
-    assert.are.equal(myHeap.pop(), 1)
-    assert.are.equal(myHeap.pop(), -1)
+    assert.are.equal(myHeap:pop(), 500)
+    assert.are.equal(myHeap:pop(), 3)
+    assert.are.equal(myHeap:pop(), 2)
+    assert.are.equal(myHeap:pop(), 1)
+    assert.are.equal(myHeap:pop(), -1)
   end)
 
   it("Push and compare objects", function()
-    myHeap.compare = function (obj_a,obj_b)
+    myHeap:setCompare(function (obj_a,obj_b)
       return obj_a.p <= obj_b.p
-    end
-    myHeap.push( {p=2 })
-    myHeap.push( {p=500} );
-    myHeap.push( {p=1} );
-    myHeap.push( {p=-1} );
-    myHeap.push( {p=3} );
+    end)
+    myHeap:push( {p=2 })
+    myHeap:push( {p=500} );
+    myHeap:push( {p=1} );
+    myHeap:push( {p=-1} );
+    myHeap:push( {p=3} );
 
-    assert.are.equal( myHeap.pop().p, -1 )
-    assert.are.equal( myHeap.pop().p, 1 )
-    assert.are.equal( myHeap.pop().p, 2 )
-    assert.are.equal( myHeap.pop().p, 3 )
-    assert.are.equal( myHeap.pop().p, 500 )
+    assert.are.equal( myHeap:pop().p, -1 )
+    assert.are.equal( myHeap:pop().p, 1 )
+    assert.are.equal( myHeap:pop().p, 2 )
+    assert.are.equal( myHeap:pop().p, 3 )
+    assert.are.equal( myHeap:pop().p, 500 )
   end)
 end)
